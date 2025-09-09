c8ff615a7f931079445ae18ca84aece5c5d31635
正岡　陽子 <yoko-masaoka@apollosystem.co.jp>
2025-09-09 15:59:50 +0900

    Gitの課題

--- helloWorld/GitTest.java
+++ helloWorld/GitTest.java
@@ -1,6 +1,5 @@
 public class GitTest {
     public static void main(String[] args) throws Exception {
-        System.out.println("Let's go from commit to push!");
         System.out.println("コミットからプッシュまでを行おう！");
     }
     
